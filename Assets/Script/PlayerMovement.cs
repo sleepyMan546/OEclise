7b638a6f5eacd9d22e553a16c0e845ec1db14ed5
sleepyMan546 <pokemondota403@gmail.com>
2025-02-11 00:29:01 +0700

    sleepmak

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -22,6 +22,9 @@ public class PlayerMovement : MonoBehaviour
     private float direction  ;
     public float moveDirection;
     public Transform shootPoint;
+    public WeaponSwitchDop weaponSwitchDop;
+    private int airJumpCount = 0; 
+    public int airJumpsAllowed = 1; 
 
     void Start()
     {
@@ -45,26 +48,41 @@ public class PlayerMovement : MonoBehaviour
         {
             bodyRenderer.flipX = true;
         }
-        
-        if (Input.GetKey(KeyCode.Space) && IsGrounded()) {
-            Jump();
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (IsGrounded())
+            {
+                Jump();
+                airJumpCount = 0; 
+                Debug.Log("airJump = " + airJumpCount);
+            }
+            else if (CanAirJump()) 
+            {
+                AirJump();
+            }
         }
         //set animation
         anim.SetBool("Walk", move != 0);
 
-        if (Input.GetKey(KeyCode.LeftShift) && HasPistrol())
+        if (Input.GetKey(KeyCode.LeftShift) && CanDash()) 
         {
             Debug.Log("Dash");
             Dash();
         }
-
+        if (Input.GetKey(KeyCode.E) )
+        {
+            Debug.Log("Can Dash" + CanDash());
+        }
 
     }
-    bool HasPistrol()
+    bool CanDash()  
     {
-        return gameObject.GetComponent<Pistol>() != null;
-        
+       
+        return weaponSwitchDop != null && weaponSwitchDop.GetCurrentWeapon() == "pistol";
+    }
+    bool CanAirJump()
+    {
+        return airJumpCount < airJumpsAllowed && weaponSwitchDop != null && weaponSwitchDop.GetCurrentWeapon() == "shotgun";
     }
     public void Jump()
     {
@@ -92,6 +110,13 @@ public class PlayerMovement : MonoBehaviour
         Vector3 scale = transform.localScale;
         scale.x *= -1;
         transform.localScale = scale;
+    }
+
+    void AirJump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce); 
+        anim.SetTrigger("Jump");  
+        airJumpCount++; 
     }
 }
     
