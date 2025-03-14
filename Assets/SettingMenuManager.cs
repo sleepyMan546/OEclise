3d267ced74948a13ab2726c669d5fd40f65c1acf
icomefromdark <pypoy66@gmail.com>
2025-03-15 00:32:34 +0700

    Resoolution setting

--- Assets/SettingMenuManager.cs
+++ Assets/SettingMenuManager.cs
@@ -1,15 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class SettingMenuManager : MonoBehaviour
 {
+    public TMP_Dropdown myDropdown;
+    public Toggle Fullscreentoggle;
+
+    Resolution[] AllResolutions;
+    bool IsFullScreen;
+    int SelectedResolution;
+    List<Resolution> SelectedResolutionList = new List<Resolution>();
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        IsFullScreen = true;
+        AllResolutions = Screen.resolutions;
 
+        List<string> resolutionStringList = new List<string>();
+        string newRes;
+        foreach (Resolution res in AllResolutions)
+        {
+            newRes = res.width.ToString() + " x " + res.height.ToString();
+            if (!resolutionStringList.Contains(newRes))
+            {
+                resolutionStringList.Add(newRes);
+                SelectedResolutionList.Add(res);
+            }
+        }
+        myDropdown.AddOptions(resolutionStringList);
+    }
+    public void ChangeResolution()
+    {
+        SelectedResolution = myDropdown.value;
+        Screen.SetResolution(SelectedResolutionList[SelectedResolution].width, SelectedResolutionList[SelectedResolution].height, IsFullScreen);
+
+    }
+    public void ChangeFullScreen()
+    {
+        SelectedResolution = myDropdown.value;
+        Screen.SetResolution(SelectedResolutionList[SelectedResolution].width, SelectedResolutionList[SelectedResolution].height, IsFullScreen);
+    }
     // Update is called once per frame
     void Update()
     {
