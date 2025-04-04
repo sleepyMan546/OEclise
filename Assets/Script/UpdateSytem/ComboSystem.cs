97153adbca9fda7c22cbf3dc9dd403f86f5828f8
sleepyMan546 <pokemondota403@gmail.com>
2025-04-04 16:56:53 +0700

    Dopsqw

--- Assets/Script/UpdateSytem/ComboSystem.cs
+++ Assets/Script/UpdateSytem/ComboSystem.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using UnityEditor.Rendering.LookDev;
+//using UnityEditor.Rendering.LookDev;
 
 public class ComboSystem : MonoBehaviour
 {
