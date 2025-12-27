7a4c5f07b11d84dadf2437b89ff27b8ee7da49b5
sleepyMan546 <pokemondota403@gmail.com>
2025-12-27 08:57:54 +0700

    FixedUpdate

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -66,11 +66,15 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    void FixedUpdate(){
+           float move = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(move * moveSpeed, rb.velocity.y);
 
+    }
+    
     void Update()
     {
-        float move = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(move * moveSpeed, rb.velocity.y);
+    
 
 
         if (facingRight)
